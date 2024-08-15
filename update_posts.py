3ac76dd2e4e43306bf989e59c6bc55b2a7c86057
LesserCat <suika@kadama.eu.org>
2024-08-15 14:51:16 +0800

    Update update_posts.py

--- update_posts.py
+++ update_posts.py
@@ -1,16 +1,16 @@
 import os
 import json
 import asyncio
-from telegram import Bot
+from telegram.ext import ApplicationBuilder
+
+os.makedirs('media', exist_ok=True)
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
 CHANNEL_ID = os.environ['CHANNEL_ID']
 
-bot = Bot(token=BOT_TOKEN)
-
-async def get_latest_posts():
+async def get_latest_posts(application):
     # 从 Telegram 获取最新消息
-    updates = await bot.get_updates(offset=-1, limit=100)
+    updates = await application.bot.get_updates(offset=-1, limit=100)
     posts = []
     for update in updates:
         message = update.channel_post
@@ -23,7 +23,7 @@ async def get_latest_posts():
             }
             if message.photo:
                 photo = message.photo[-1]
-                file = await bot.get_file(photo.file_id)
+                file = await application.bot.get_file(photo.file_id)
                 image_path = f'media/{message.message_id}.jpg'
                 await file.download_to_drive(image_path)
                 post['images'].append({
@@ -45,13 +45,14 @@ def update_posts_file(new_posts):
     # 合并新旧帖子并去重
     all_posts = existing_posts + new_posts
     unique_posts = {post['id']: post for post in all_posts}.values()
-    sorted_posts = sorted(unique_posts, key=lambda x: x['date'])
+    sorted_posts = sorted(unique_posts, key=lambda x: x['date'], reverse=True)
 
     with open('posts.json', 'w') as f:
         json.dump(sorted_posts, f, indent=2)
 
 async def main():
-    new_posts = await get_latest_posts()
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    new_posts = await get_latest_posts(application)
     update_posts_file(new_posts)
 
 if __name__ == '__main__':
