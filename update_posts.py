cb05f4b5578164a88cc837932b77024853d3c5e5
LesserCat <suika@kadama.eu.org>
2024-08-15 14:41:19 +0800

    Create update_posts.py

---
+++ update_posts.py
@@ -0,0 +1,55 @@
+import os
+import json
+import requests
+from telegram import Bot
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+
+bot = Bot(token=BOT_TOKEN)
+
+def get_latest_posts():
+    # 从 Telegram 获取最新消息
+    updates = bot.get_updates(offset=-1, limit=100)
+    posts = []
+    for update in updates:
+        message = update.channel_post
+        if message and str(message.chat.id) == CHANNEL_ID:
+            post = {
+                'id': message.message_id,
+                'date': message.date.isoformat(),
+                'text': message.text or '',
+                'images': []
+            }
+            if message.photo:
+                photo = message.photo[-1]
+                file = bot.get_file(photo.file_id)
+                image_path = f'media/{message.message_id}.jpg'
+                file.download(image_path)
+                post['images'].append({
+                    'width': photo.width,
+                    'height': photo.height,
+                    'url': image_path,
+                    'size': os.path.getsize(image_path)
+                })
+            posts.append(post)
+    return posts
+
+def update_posts_file(new_posts):
+    if os.path.exists('posts.json'):
+        with open('posts.json', 'r') as f:
+            existing_posts = json.load(f)
+    else:
+        existing_posts = []
+
+    # 合并新旧帖子并去重
+    all_posts = existing_posts + new_posts
+    unique_posts = {post['id']: post for post in all_posts}.values()
+    sorted_posts = sorted(unique_posts, key=lambda x: x['date'])
+
+    with open('posts.json', 'w') as f:
+        json.dump(sorted_posts, f, indent=2)
+
+if __name__ == '__main__':
+    new_posts = get_latest_posts()
+    update_posts_file(new_posts)
